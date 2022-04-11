42e30dab2fd796126a41916da8720273416fea85
Asegidew Kefyalew <Kasegidew@gmail.com>
2022-04-11 13:37:15 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Prints a random number and states whether
  *         it is positive, negative, or zero.
- *
+ * 
  * Return: Always 0.
  */
 int main(void)
@@ -25,29 +25,3 @@ printf("%d is zero\n", n);
 
 return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
